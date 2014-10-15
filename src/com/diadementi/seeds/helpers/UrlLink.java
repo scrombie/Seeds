1e8362890299428a5f3ba643d1619200a4daca4b
maxdeviper <mxvmayaki@gmail.com>
2014-10-15 21:58:51 +0100

    changed url to site domain

--- src/com/diadementi/seeds/helpers/UrlLink.java
+++ src/com/diadementi/seeds/helpers/UrlLink.java
@@ -2,17 +2,17 @@ package com.diadementi.seeds.helpers;
 
 public  class UrlLink {
 
-	public static	final	String featured="http://192.168.56.1/seeds3/v2/featured";
-	public final static	String  Campaigns="http://192.168.43.146/seeds3/v2/campaigns";
-	public static final	String add="http://192.168.56.1/seeds3/v2/ncampaign";
-	public static final String update ="http://192.168.56.1/seeds3/v2/campaign/" ;
-	public static final String delete = "http://192.168.56.1/seeds3/v2/campaign/";
-	private static String campaign="http://192.168.43.146/seeds3/v2/campaign/";
-	private static String category="http://192.168.43.146/seeds3/v2/category/";
-	public static String catergories="http://192.168.43.146/seeds3/v2/categories";
-	public static final String register="http://192.168.56.1/seeds3/v2/register";
-	public static final String login="http://192.168.56.1/seeds3/v2/login";
-	public static final String usersCampaign="http://192.168.56.1/seeds3/v2/mycampaign";
+	public static	final	String featured="http://seeds.diadementi.com/v2/featured";
+	public final static	String  Campaigns="http://seeds.diadementi.com/v2/campaigns";
+	public static final	String add="http://seeds.diadementi.com/v2/ncampaign";
+	public static final String update ="http://seeds.diadementi.com/v2/campaign/" ;
+	public static final String delete = "http://seeds.diadementi.com/v2/campaign/";
+	private static String campaign="http://seeds.diadementi.com/v2/campaign/";
+	private static String category="http://seeds.diadementi.com/v2/category/";
+	public static String catergories="http://seeds.diadementi.com/v2/categories";
+	public static final String register="http://seeds.diadementi.com/v2/register";
+	public static final String login="http://seeds.diadementi.com/v2/login";
+	public static final String usersCampaign="http://seeds.diadementi.com/v2/mycampaign";
 	public static String getCategory(int id) {
 		return category+id;
 	}
